a2f2b50bc8422c28e42a2bc4c96872b5084c8b40
DylanO <dylan6765@gmail.com>
2024-09-23 11:22:23 -0400

    added an email field for uoft emails and updated header to display uoft email. Added error checking to email field if empty, if non email, and if non uoft email

--- hello.py
+++ hello.py
@@ -4,23 +4,36 @@ from flask_moment import Moment
 from datetime import datetime
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, ValidationError
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 app.config['SECRET_KEY'] = 'hard to guess string'
 
+def email_check(form, field):
+    if '@' not in field.data:
+        raise ValidationError("Please include an '@' in the email address. '{}' is missing an '@'.".format(field.data))
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
     form = NameForm()
     if form.validate_on_submit():
         old_name = session.get('name')
+        old_email = session.get('email')
         if old_name is not None and old_name != form.name.data:
             flash('Looks like you have changed your name!')
+        if old_email is not None and old_email != form.email.data:
+            flash('Looks like you have changed your email!')
+
         session['name'] = form.name.data
+
+        if 'utoronto' in form.email.data:
+            session['email'] = form.email.data
+        else:
+            session['email'] = None
         return redirect(url_for('index'))
-    return render_template('index.html', form=form, name=session.get('name'))
+    return render_template('index.html', form=form, name=session.get('name'), email=session.get('email'))
 
 
 @app.route("/user/<name>")
@@ -37,4 +50,5 @@ def internal_server_error(e):
 
 class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[DataRequired()])
+    email = StringField('What is your UofT Email address?', validators=[email_check, DataRequired()])
     submit = SubmitField('Submit')
